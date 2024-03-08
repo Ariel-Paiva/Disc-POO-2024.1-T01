5ea2005c62a3e6156c4cd42a2663f6d67edccbba
Gabriel <159455478+Bilinhas@users.noreply.github.com>
2024-03-08 00:31:07 -0300

    Questão 11

---
+++ CAP-03/Exercicios-Propostos/QP11/src/br/edu/principal/Principal.java
@@ -0,0 +1,19 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Área do losango."+"\n"+"Digite, em metros, o valor da diagonal maior do losango.");
+		float dmaior = sc.nextFloat();
+		System.out.println("Agora digite o valor da diagonal menor do losango.");
+		float dmenor = sc.nextFloat();
+		float a = (dmaior * dmenor)/2;
+
+		System.out.println("A área do losango é: "+a+"m²");
+	}
+
+}
